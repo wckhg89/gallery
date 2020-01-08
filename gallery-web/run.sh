d30f7c3d95d4cbe82a69985032f5d9afe0c58dc4
wckhg89 <wckhg89@gmail.com>
2020-01-08 09:42:05 +0900

    fix - docker-compose mongo auth

--- gallery-web/run.sh
+++ gallery-web/run.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-docker-compose up -d --build
+docker-compose up -d --build --force-recreate
--- gallery-web/stop.sh
+++ gallery-web/stop.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-docker-compose stop
+docker-compose down
