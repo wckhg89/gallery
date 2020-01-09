10fadc3c1bbaebb544e4f45f84ab5fe52f42c98c
wckhg89 <wckhg89@gmail.com>
2020-01-09 09:11:02 +0900

    add - aws init script

---
+++ gallery-web/aws-infra.sh
@@ -0,0 +1,28 @@
+# java 8
+sudo yum install -y java-1.8.0-openjdk-devel.x86_64
+sudo /usr/sbin/alternatives --config java
+
+sudo yum remove java-1.7.0-openjdk
+java -version
+
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 10.16.3
+
+
+# docker
+sudo yum install -y docker
+service docker start
+
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+
