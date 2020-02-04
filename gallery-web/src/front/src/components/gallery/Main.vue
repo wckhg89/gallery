d76293f992a628eec234c6649942ce6179cc9de9
wckhg89 <wckhg89@gmail.com>
2020-02-04 18:04:46 +0900

    update

--- gallery-web/src/front/src/components/gallery/Main.vue
+++ gallery-web/src/front/src/components/gallery/Main.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="image-div">
-        <router-link to="/episode/1">
-            <el-image class="image-item"
-                      :src="url"
-                      fit="fill"
-                      :lazy=true>
-                <div slot="error" class="image-slot">
-                    <i class="el-icon-picture-outline"></i>
-                </div>
-            </el-image>
-        </router-link>
+    <div>
+        <div class="image-div">
+            <router-link to="/episode/1">
+                <el-image class="image-item"
+                          :src="url"
+                          fit="fill"
+                          :lazy=true>
+                    <div slot="error" class="image-slot">
+                        <i class="el-icon-picture-outline"></i>
+                    </div>
+                </el-image>
+            </router-link>
+        </div>
+        <div class="text-div">
+            4月 12日
+        </div>
     </div>
-
 </template>
 
 <script>
@@ -20,8 +24,9 @@
         data() {
             return {
                 url: undefined,
-                mobileImageUrl: 'https://d2i2o4t1c9odyj.cloudfront.net/main_img.jpg',
-                desktopImageUrl: 'https://d2i2o4t1c9odyj.cloudfront.net/main_img.jpg',
+                // mobileImageUrl: 'https://d2i2o4t1c9odyj.cloudfront.net/main_img.jpg',
+                mobileImageUrl: 'https://d2i2o4t1c9odyj.cloudfront.net/main_img_gallery.jpeg',
+                desktopImageUrl: 'https://d2i2o4t1c9odyj.cloudfront.net/main_img_gallery.jpeg',
             }
         },
         methods: {
@@ -47,9 +52,12 @@
 <style scoped>
     .image-div {
         text-align: center;
-        margin: 20px 10% 50px 10%;
+        margin: 20px 10% 35px 10%;
     }
-    .image-item {
-        /*width: 100%;*/
+    .text-div {
+        font-size: 14px;
+        font-weight: bold;
+        text-align: center;
+        color: #F56C6C;
     }
 </style>
